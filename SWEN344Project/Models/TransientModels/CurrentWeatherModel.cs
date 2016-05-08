09350a6a8c8b32ee871d535b4cfbd8c8d2196f7f
iis2696 <iis2696@rit.edu>
2016-05-07 21:02:23 -0400

    Updated Publish Profiles

--- SWEN344Project/Models/TransientModels/CurrentWeatherModel.cs
+++ SWEN344Project/Models/TransientModels/CurrentWeatherModel.cs
@@ -42,6 +42,7 @@ namespace SWEN344Project.Models.TransientModels
             public decimal temp_min { get; set; }
             public decimal temp_max { get; set; }
             public decimal pressure { get; set; }
+            public decimal humidity { get; set; }
 
             public decimal? grnd_level { get; set; }
             public decimal? sea_level { get; set; }
